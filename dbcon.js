359b41225703ef01c63d26f4585d0fd17bfb717c
Diane Nguyen <diane.nguyen.124@gmail.com>
2020-03-26 20:23:40 -0700

    dbcon pointing to cleardb database

--- dbcon.js
+++ dbcon.js
@@ -1,14 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_nguydian',
-  password        : '7408',
-  database        : 'cs340_nguydian',
-  // host            : 'us-cdbr-iron-east-04.cleardb.net',
-  // user            : 'b958413ff9a5ad',
-  // password        : 'edfa40d9',
-  // database        : 'heroku_30ffccaec8e3a1a',
+  host            : 'us-cdbr-iron-east-04.cleardb.net',
+  user            : 'b958413ff9a5ad',
+  password        : 'edfa40d9',
+  database        : 'heroku_30ffccaec8e3a1a',
   multipleStatements: true
 });
 
